28a0748061428ab46f43d24cb53af74554b7a853
Sudipta Basak <sudiptab2100@gmail.com>
2024-01-21 17:29:47 +0530

    Download by date working

---
+++ downloader.py
@@ -0,0 +1,18 @@
+import requests
+import shutil
+
+
+def date_to_url(ddmmyy):
+    return f'https://www.bseindia.com/download/BhavCopy/Equity/EQ{ddmmyy}_CSV.ZIP'
+
+def download_by_date(ddmmyy):
+    url = date_to_url(ddmmyy)
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    
+    with open(f"files/zips/EQ{ddmmyy}.zip", 'wb') as file:
+        file.write(response.content)
+    
+    shutil.unpack_archive(f"files/zips/EQ{ddmmyy}.zip", f"files/csvs/")
+
+download_by_date("120124")
