f0ea43941bd7e8cc52ab38acca815aa7145fd3ec
Sudipta Basak <sudiptab2100@gmail.com>
2024-01-24 21:55:25 +0530

    Added db operation store and remove

---
+++ db.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from pymongo import MongoClient
+
+
+def load_csv_by_date(ddmmyy):
+    path = f"files/csvs/EQ{ddmmyy}.csv"
+    df = pd.read_csv(path)
+    
+    datas = []
+    for index, row in df.iterrows():
+        data = {
+            "code": row["SC_CODE"],
+            "name": row["SC_NAME"].strip(),
+            "open": row["OPEN"],
+            "high": row["HIGH"],
+            "low": row["LOW"],
+            "close": row["CLOSE"]
+        }
+        datas.append(data)
+    
+    return datas
+
+def store_data(ddmmyy):
+    datas = load_csv_by_date(ddmmyy)
+    
+    collection = db[f"EQ{ddmmyy}"]
+    
+    collection.insert_many(datas)
+    
+    client.close()
+
+def remove_data(ddmmyy):
+    collection = db[f"EQ{ddmmyy}"]
+    
+    collection.drop()
+    
+    client.close()
+
+
+client = MongoClient()
+db = client["bse"]
