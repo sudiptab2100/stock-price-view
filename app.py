3381e3fc96c864071b9e2ffc9cb8d2a0aa9222d2
Sudipta Basak <sudiptab2100@gmail.com>
2024-01-27 02:14:11 +0530

    add & remove favourites post request added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from preset import preset
-from db import get_top_k, get_favourites
+from db import get_top_k, get_favourites, add_favourite, remove_favourite
 import uvicorn
 
 
@@ -13,6 +13,16 @@ def get_tops(k: int):
 @app.get("/get_favourites")
 def get_favs():
     return get_favourites()
+
+@app.post("/add_favourite/{stock_code}")
+def add_fav(stock_code: int):
+    add_favourite(stock_code)
+    return {"message": "Added to favourites"}
+
+@app.delete("/remove_favourite/{stock_code}")
+def remove_fav(stock_code: int):
+    remove_favourite(stock_code)
+    return {"message": "Removed from favourites"}
 
 
 if __name__ == "__main__":
